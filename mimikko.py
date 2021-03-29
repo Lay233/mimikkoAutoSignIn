d690efb3f6cc88de0ac5d0331fe907575435b1a7
Schwi <39186981+cyb233@users.noreply.github.com>
2021-03-29 19:07:21 +0700

    Update mimikko.py

--- mimikko.py
+++ mimikko.py
@@ -80,6 +80,9 @@ try:
             if a.strip() in ['1', '2', '3', '4', '5', '6', '7']:
                 resign = a.strip()
                 print("resign开启")
+            elif a.strip() > 7:
+                resign = 7
+                print("resign开启")
             else:
                 print("resign关闭")
         if o == '-l':
